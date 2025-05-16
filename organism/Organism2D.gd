fb984c6d166eadb7634d3bb34d5f3d05f1dcab58
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-05-16 13:30:01 +0300

    Organism2D WIP

---
+++ organism/Organism2D.gd
@@ -0,0 +1,19 @@
+class_name Organism2D extends CharacterBody2D
+## TODO desc
+
+signal moved_to_point
+signal moved_to_organism
+
+var params: Resource # Should make custom resource
+
+
+func move_to_point(point: Vector2) -> void:
+	pass
+
+
+func move_to_organism(organism: Organism) -> void:
+	pass
+
+
+func move() -> void:
+	pass
