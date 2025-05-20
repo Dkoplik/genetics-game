1806355517f1fbf8b684b6b587c5ac794fd40e10
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-05-20 22:12:35 +0300

    Update local effect

--- local-effect/local-effect.gd
+++ local-effect/local-effect.gd
@@ -1,7 +1,16 @@
 class_name LocalEffect extends Node2D
+## Применяет эффект на [Organism] в пределах своей зоны действия.
 
+## Эффект.
 @export var summand: String
+## Переменные эффекта.
 @export var variables: PackedStringArray
+## Цвет эффекта.
+@export var color := Color("ffffff64")
+
+
+func _ready() -> void:
+	$Sprite2D.modulate = color
 
 
 func _on_organism_detector_organism_entered(organism: Organism) -> void:
--- local-effect/organism-detector.gd
+++ local-effect/organism-detector.gd
@@ -1,4 +1,5 @@
 class_name OrganismDetector extends Area2D
+## Обнаруживает пересечения с [Organism].
 
 signal organism_entered(organism: Organism)
 signal organism_exited(organism: Organism)
@@ -14,7 +15,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.get_parent() is not Organism:
 		push_error("У Organism2D родитель не Organism")
 		return
-	organism_entered.emit(body.get_parent())
+	organism_entered.emit(body.get_parent() as Organism)
 
 
 func _on_body_exited(body: Node2D) -> void:
@@ -23,4 +24,4 @@ func _on_body_exited(body: Node2D) -> void:
 	if body.get_parent() is not Organism:
 		push_error("У Organism2D родитель не Organism")
 		return
-	organism_exited.emit(body.get_parent())
+	organism_exited.emit(body.get_parent() as Organism)
