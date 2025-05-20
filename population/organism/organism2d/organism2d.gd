e2ce933c7893440201430e1e9d08b2561b49ed79
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-05-20 21:47:26 +0300

    Update Organism2D

--- population/organism/organism2d/organism2d.gd
+++ population/organism/organism2d/organism2d.gd
@@ -1,23 +1,29 @@
 class_name Organism2D extends CharacterBody2D
-## TODO desc
+## Отвечает за поведение и положение организма в 2D пространстве.
 
 signal canceled_moving_to_organism(target: Organism2D)
+signal canceled_moving_to_point(point: Vector2)
 signal moved_to_organism2d(target: Organism2D)
+signal moved_to_point(point: Vector2)
 
+## Основные параметры.
 var params: Organism2DParams = preload("./organism2d-default-params.tres")
-
-var _is_moving_to_point := false
-var _target_point: Vector2
 
 var _is_moving_to_organism := false
 var _target_organism: Organism2D
 
-@onready var _shape := ($CollisionShape2D as CollisionShape2D).shape as CircleShape2D
+var _is_moving_to_point := false
+var _target_point: Vector2
+
+var _is_randomly_moving := false
+
+## Форма организма (коллизия).
+var _shape: CircleShape2D
 
 
 func _ready() -> void:
-	_target_point = get_random_point()
-	_is_moving_to_point = true
+	_shape = ($CollisionShape2D as CollisionShape2D).shape as CircleShape2D
+	start_random_movement()
 
 
 func _physics_process(delta: float) -> void:
@@ -26,14 +32,57 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+## Начать движение к случайным точкам.
+func start_random_movement() -> void:
+	if _is_randomly_moving:
+		push_warning("Организм уже движется в случайных направлениях")
+		return
+	_is_randomly_moving = true
+	_move_to_random_point()
+	moved_to_point.connect(_move_to_random_point)
+
+
+## Прекратить движение к случайным точкам.
+func stop_random_movement() -> void:
+	if not _is_randomly_moving:
+		push_warning("Случайное движение уже остановлено")
+		return
+	_is_randomly_moving = false
+	if _is_moving_to_point:
+		canceled_moving_to_point.emit(_target_point)
+	_is_moving_to_point = false
+	moved_to_point.disconnect(_move_to_random_point)
+
+
+## Начать движение к организму [param organism]. Если до этого осуществлял
+## движение до другой цели, то испускает соответствующий сигнал об отмене
+## и меняет цель.
 func move_to_organism2d(organism: Organism2D) -> void:
-	# До этого двигался к другому
-	if _is_moving_to_organism:
-		canceled_moving_to_organism.emit()
+	if _is_randomly_moving:
+		stop_random_movement()
+	if _is_moving_to_organism: # До этого двигался к другому организму
+		canceled_moving_to_organism.emit(_target_organism)
 	_is_moving_to_organism = true
 	_target_organism = organism
 
 
+## Начать движение к точке [param point]. Если до этого осуществлял движение до
+## другой цели, то испускает соответствующий сигнал об отмене и меняет цель.
+## Если [param save_target_organism] = true, то не сбрасывает целевой организм.
+func move_to_point(point: Vector2, save_target_organism := false) -> void:
+	if _is_randomly_moving:
+		stop_random_movement()
+	# До этого двигался к другому организму
+	if _is_moving_to_organism and not save_target_organism:
+		canceled_moving_to_organism.emit(_target_organism)
+	if _is_moving_to_point: # До этого двигался к другой точке
+		canceled_moving_to_point.emit(_target_point)
+	_is_moving_to_organism = false
+	_is_moving_to_point = true
+	_target_point = point
+
+
+## Случайная точка в пределах границ мира, указанных в [member params].
 func get_random_point() -> Vector2:
 	return Vector2(
 		randf_range(params.world_left_border, params.world_right_border),
@@ -41,20 +90,20 @@ func get_random_point() -> Vector2:
 	)
 
 
+## Обработать движение до организма. Завязано на движении к точке, поэтому
+## должно вызываться до [member _moving_to_point].
 func _moving_to_organism() -> void:
 	if not _is_moving_to_organism:
 		return
 
-	_target_point = _target_organism.global_position
-	_is_moving_to_point = true
-
+	move_to_point(_target_organism.global_position, true)
 	var dist: float = global_position.distance_to(_target_organism.global_position)
-	if dist <= params.move_to_organism_distance + _shape.radius:
+	if dist <= params.move_to_organism_dist_coef * _shape.radius:
 		_is_moving_to_organism = false
 		moved_to_organism2d.emit(_target_organism)
-		_target_point = get_random_point()
 
 
+## Обработать движение до точки.
 func _moving_to_point(delta: float) -> void:
 	if not _is_moving_to_point:
 		return
@@ -62,8 +111,13 @@ func _moving_to_point(delta: float) -> void:
 	var diff : Vector2 = _target_point - global_position
 	var direction: Vector2 = diff.normalized()
 	velocity = params.speed * direction
-
 	if (velocity * delta).length() > diff.length():
 		velocity = Vector2.ZERO
 		global_position = _target_point
-		_target_point = get_random_point()
+		moved_to_point.emit(_target_point)
+
+
+## Назначить движение к случайной точке.
+func _move_to_random_point() -> void:
+	_target_point = get_random_point()
+	_is_moving_to_point = true
