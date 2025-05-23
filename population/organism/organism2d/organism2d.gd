c2d4357eb22573c99a7da85a473a0d74b9b53cb3
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-05-23 12:14:07 +0300

    Update organism2d.gd

--- population/organism/organism2d/organism2d.gd
+++ population/organism/organism2d/organism2d.gd
@@ -6,16 +6,14 @@ signal canceled_moving_to_point(point: Vector2)
 signal moved_to_organism2d(target: Organism2D)
 signal moved_to_point(point: Vector2)
 
+enum STATE {IDLE, MOVING_TO_POINT, MOVING_TO_ORGANISM, WANDERING}
+
 ## Основные параметры.
 var params: Organism2DParams = preload("./organism2d-default-params.tres")
 
-var _is_moving_to_organism := false
-var _target_organism: Organism2D
-
-var _is_moving_to_point := false
+var _cur_state: STATE = STATE.IDLE
+var _target_organism: Organism2D = null
 var _target_point: Vector2
-
-var _is_randomly_moving := false
 
 ## Форма организма (коллизия).
 var _shape: CircleShape2D
@@ -23,7 +21,7 @@ var _shape: CircleShape2D
 
 func _ready() -> void:
 	_shape = ($CollisionShape2D as CollisionShape2D).shape as CircleShape2D
-	start_random_movement()
+	start_wandering()
 
 
 func _physics_process(delta: float) -> void:
@@ -33,98 +31,76 @@ func _physics_process(delta: float) -> void:
 
 
 ## Начать движение к случайным точкам.
-func start_random_movement() -> void:
-	if _is_randomly_moving:
-		push_warning("Организм уже движется в случайных направлениях")
-		return
-	if _is_moving_to_organism:
-		canceled_moving_to_organism.emit(_target_organism)
-		_is_moving_to_organism = false
-	if _is_moving_to_point:
-		canceled_moving_to_point.emit(_target_point)
-		_is_moving_to_point = false
-	_is_randomly_moving = true
+func start_wandering() -> void:
+	_change_state(STATE.WANDERING)
 	_move_to_random_point()
 	moved_to_point.connect(_move_to_random_point)
 
 
 ## Прекратить движение к случайным точкам.
-func stop_random_movement() -> void:
-	if not _is_randomly_moving:
+func stop_wandering() -> void:
+	if _cur_state != STATE.WANDERING:
 		push_warning("Случайное движение уже остановлено")
 		return
-	_is_randomly_moving = false
-	if _is_moving_to_point:
-		canceled_moving_to_point.emit(_target_point)
-	_is_moving_to_point = false
 	moved_to_point.disconnect(_move_to_random_point)
+	_cur_state = STATE.IDLE
 
 
 ## Начать движение к организму [param organism]. Если до этого осуществлял
 ## движение до другой цели, то испускает соответствующий сигнал об отмене
 ## и меняет цель.
 func move_to_organism2d(organism: Organism2D) -> void:
-	if _is_randomly_moving:
-		stop_random_movement()
-	if _is_moving_to_organism:  # До этого двигался к другому организму
-		canceled_moving_to_organism.emit(_target_organism)
-	_is_moving_to_organism = true
+	_change_state(STATE.MOVING_TO_ORGANISM)
 	_target_organism = organism
 
 
 ## Начать движение к точке [param point]. Если до этого осуществлял движение до
 ## другой цели, то испускает соответствующий сигнал об отмене и меняет цель.
-## Если [param save_target_organism] = true, то не сбрасывает целевой организм.
-func move_to_point(point: Vector2, save_target_organism := false) -> void:
-	if _is_randomly_moving:
-		stop_random_movement()
-	# До этого двигался к другому организму
-	if _is_moving_to_organism and not save_target_organism:
-		canceled_moving_to_organism.emit(_target_organism)
-		_is_moving_to_organism = false
-	if _is_moving_to_point:  # До этого двигался к другой точке
-		canceled_moving_to_point.emit(_target_point)
-	_is_moving_to_point = true
+func move_to_point(point: Vector2) -> void:
+	_change_state(STATE.MOVING_TO_POINT)
 	_target_point = point
 
 
+func get_current_target() -> Variant:
+	match _cur_state:
+		STATE.MOVING_TO_ORGANISM:
+			return _target_organism
+		STATE.MOVING_TO_POINT:
+			return _target_point
+	return null
+
+
 ## Случайная точка в пределах границ мира, указанных в [member params].
-func get_random_point() -> Vector2:
+func _get_random_point() -> Vector2:
 	return Vector2(
 		randf_range(params.world_left_border, params.world_right_border),
 		randf_range(params.world_upper_border, params.world_lower_border)
 	)
 
 
-func get_current_target() -> Variant:
-	if _is_moving_to_organism:
-		return _target_organism
-	if _is_moving_to_point:
-		return _target_point
-	return null
-
-
 ## Обработать движение до организма. Завязано на движении к точке, поэтому
 ## должно вызываться до [member _moving_to_point].
 func _moving_to_organism() -> void:
-	if not _is_moving_to_organism:
+	if _cur_state != STATE.MOVING_TO_ORGANISM:
 		return
 
 	# Цели больше не существует.
 	if not _target_organism:
-		start_random_movement()
+		_target_organism = null
+		start_wandering()
 		return
 
-	move_to_point(_target_organism.global_position, true)
-	var dist: float = global_position.distance_to(_target_organism.global_position)
+	_target_point = _target_organism.global_position
+	var dist: float = global_position.distance_to(_target_point)
 	if dist <= params.move_to_organism_dist_coef * _shape.radius:
-		_is_moving_to_organism = false
 		moved_to_organism2d.emit(_target_organism)
+		_cur_state = STATE.IDLE # dont trigger cancelation of target
+		start_wandering()
 
 
 ## Обработать движение до точки.
 func _moving_to_point(delta: float) -> void:
-	if not _is_moving_to_point:
+	if _cur_state == STATE.IDLE:
 		return
 
 	var diff: Vector2 = _target_point - global_position
@@ -133,11 +109,29 @@ func _moving_to_point(delta: float) -> void:
 	if (velocity * delta).length() > diff.length():
 		velocity = Vector2.ZERO
 		global_position = _target_point
-		if _is_moving_to_point:
+		if _cur_state == STATE.MOVING_TO_POINT or _cur_state == STATE.WANDERING:
 			moved_to_point.emit(_target_point)
 
 
 ## Назначить движение к случайной точке.
 func _move_to_random_point(_previous_point := Vector2.ZERO) -> void:
-	_target_point = get_random_point()
-	_is_moving_to_point = true
+	_target_point = _get_random_point()
+
+
+## Change state with all signals, when needed.
+func _change_state(new_state: STATE) -> void:
+	match _cur_state:
+		STATE.WANDERING:
+			stop_wandering()
+		STATE.MOVING_TO_ORGANISM:
+			canceled_moving_to_organism.emit(_target_organism)
+		STATE.MOVING_TO_POINT:
+			canceled_moving_to_point.emit(_target_point)
+
+	if _cur_state == new_state:
+		match _cur_state:
+			STATE.WANDERING:
+				push_warning("Organism is already wandering")
+			STATE.IDLE:
+				push_warning("Organism is already idle")
+	_cur_state = new_state
