b04643adc12638e04fcf175be1b1e3d983270450
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-05-24 09:44:06 +0300

    update local effects

--- local-effect/local-effect.gd
+++ local-effect/local-effect.gd
@@ -10,25 +10,40 @@ class_name LocalEffect extends Node2D
 ## Длительность эффекта.
 @export var duration: float = 15.0
 
+var organisms: Dictionary[Organism, Variant] = {} # Set
+
 @onready var sprite := $Sprite2D as Sprite2D
 @onready var progress_bar := $ProgressBar as ProgressBar
+@onready var detector := $OrganismDetector as OrganismDetector
 
 
 func _ready() -> void:
 	sprite.modulate = color
 	progress_bar.max_value = duration
 	progress_bar.value = duration
+	detector.organism_entered.connect(_on_organism_detector_organism_entered)
+	detector.organism_exited.connect(_on_organism_detector_organism_exited)
 
 
 func _physics_process(delta: float) -> void:
 	progress_bar.value -= delta
-	if progress_bar.value <= 0.0:
-		queue_free()
+	if progress_bar.value > 0.0:
+		return
+
+	detector.organism_entered.disconnect(_on_organism_detector_organism_entered)
+	detector.organism_exited.disconnect(_on_organism_detector_organism_exited)
+	for organism: Organism in organisms.keys():
+		organism.fitness_function.remove_summand(summand, variables)
+	organisms.clear()
+
+	queue_free()
 
 
 func _on_organism_detector_organism_entered(organism: Organism) -> void:
 	organism.fitness_function.add_summand(summand, variables)
+	organisms.set(organism, null)
 
 
 func _on_organism_detector_organism_exited(organism: Organism) -> void:
 	organism.fitness_function.remove_summand(summand, variables)
+	organisms.erase(organism)
