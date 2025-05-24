f98f9fc55bd38a346b1ab219ed5d7c1d2b41fff8
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-05-24 18:41:24 +0300

    Update UI

--- ui/info-panel/info-panel.gd
+++ ui/info-panel/info-panel.gd
@@ -2,13 +2,12 @@ class_name InfoPanel extends Control
 
 var target_organism: Organism = null
 
-@onready var hp_label: InfoLabel = $VBoxContainer/HP
-@onready var target_label: InfoLabel = $VBoxContainer/Target
-@onready var genome_label: InfoLabel = $VBoxContainer/Genome
+@onready var hp_label: InfoLabel = $VBoxContainer/HBoxContainer/VBoxContainer/HP
+@onready var genome_label: InfoLabel = $VBoxContainer/HBoxContainer/VBoxContainer_2/Genome
 @onready var ff_label: InfoLabel = $VBoxContainer/FitnessFunction
-@onready var alive_label: InfoLabel = $VBoxContainer/TimeAlive
-@onready var mutations_label: InfoLabel = $VBoxContainer/Mutations
-@onready var children_label: InfoLabel = $VBoxContainer/Children
+@onready var alive_label: InfoLabel = $VBoxContainer/HBoxContainer/VBoxContainer/TimeAlive
+@onready var mutations_label: InfoLabel = $VBoxContainer/HBoxContainer/VBoxContainer/Mutations
+@onready var children_label: InfoLabel = $VBoxContainer/HBoxContainer/VBoxContainer/Children
 
 
 func _physics_process(_delta: float) -> void:
@@ -18,7 +17,6 @@ func _physics_process(_delta: float) -> void:
 
 func _update_info(organism: Organism) -> void:
 	hp_label.data = "%0.2f" % organism.get_hp()
-	target_label.data = str(organism.behaviour.get_current_target())
 	genome_label.data =_format_dict(organism.genome.get_param_dict())
 	ff_label.data = organism.fitness_function.get_string_function()
 	alive_label.data = "%d" % organism.get_time_alive() + " sec"
